5fc60c193920228778cacf4a583313893be7df3d
Alexandr Sidorenkov <alexandr-sidorenkov@yandex.ru>
2022-05-18 08:03:59 +0400

    Домашнее задание к теме 3. "Расширенная работа с массивами". С правками по рекомендациям проверяющего.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,19 +2,13 @@
 //Task 1
 function compareArrays(arr1, arr2) {
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
+  return (arr1.length === arr2.length) && arr1.every((item, idx) => item === arr2[idx]);
 
-  let result = arr1.every((item, idx) => item === arr2[idx]);
-
-  return result; // boolean
 }
 
 // Task 2
 function advancedFilter(arr) {
 
-  let resultArr = arr.filter((item) => (item > 0) & (item % 3 === 0)).map((item) => item * 10);
+  return arr.filter((item) => (item > 0) && (item % 3 === 0)).map((item) => item * 10);
 
-  return resultArr; // array
 }
