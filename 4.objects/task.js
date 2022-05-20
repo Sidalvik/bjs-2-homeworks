a6aa4b0acde2ae999cb090601b5655be3fcf0198
Alexandr Sidorenkov <alexandr-sidorenkov@yandex.ru>
2022-05-20 23:34:21 +0400

    Домашнее задание к теме 4. Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    }
+  if (mark !== undefined) {
+   return this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...arr) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    }
+  
+  if (Array.isArray(arr)) {
+   return this.marks.push(...arr);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((sum, item, idx, arr) => {
+    if (idx === arr.length - 1) {
+      return (sum + item) / arr.length;
+      } else {
+      return sum + item;
+      }
+  });
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
