662780e5b21f3df77127a3873f2b1972a3ed669f
Alexandr Sidorenkov <alexandr-sidorenkov@yandex.ru>
2022-06-04 21:04:44 +0400

    Домашнее задание к теме 8 "Декораторы, ..., call, apply"

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,56 @@
+'use strict'
+
+// Task 1
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  return function (...args) {
+    const hash = args.join(',');
+    const index = cache.findIndex((item, idx) => item.hash === hash);
+      if (index === -1) {
+        const value = func(...args);
+        // console.log('Вычисляем: ' + value);
+        cache.push({hash, value});
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        return "Вычисляем: " + value;
+      } else {
+          // console.log('Из кэша: ' + cache[index].value);
+          return "Из кэша: " + cache[index].value;
+      }
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+// Task 2
+function debounceDecoratorNew(func, ms) {
+  let isDelayed = false;
+
+  return function (...args) {
+    if (!isDelayed) {
+      isDelayed = true;
+      setTimeout(() => isDelayed = false, ms);
+      return func(...args);
+    }
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+// Task 3
+function debounceDecorator2(func, ms) {
+  let isDelayed = false;
+
+  function wrapper (...args) {
+    wrapper.count++;
+
+    if (!isDelayed) {
+      isDelayed = true;
+      setTimeout(() => isDelayed = false, ms);
+      return func(...args);
+    }
+  }
+
+  wrapper.count = 0;
+  return wrapper;
 }
